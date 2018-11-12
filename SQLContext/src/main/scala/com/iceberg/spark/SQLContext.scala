6c6e555c59e8ca1ba5cad3c06fa2ae7cacf607c0
vaderwang <2953381201@qq.com>
2018-11-12 13:55:41 +0800

    sparkSQL

---
+++ SQLContext/src/main/scala/com/iceberg/spark/SQLContext.scala
@@ -0,0 +1,33 @@
+package com.iceberg.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+
+import org.apache.spark.SparkConf
+
+object SQLContext {
+
+  def main(args: Array[String]): Unit = {
+    val path = "test.json"
+
+    //1)创建相应的Context
+    val sparkConf = new SparkConf()
+
+    //在测试或者生产中，AppName和Master我们是通过脚本进行指定
+    sparkConf.setAppName("SQLContextApp").setMaster("spark://localhost:7077")
+
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new SQLContext(sc)
+
+    //2)相关的处理: json
+    val people = sqlContext.read.format("json").load(path)
+    people.printSchema()
+    people.show()
+
+
+
+    //3)关闭资源
+    sc.stop()
+  }
+
+}
